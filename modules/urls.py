a16937066e26491fcb32873b52601b39133fb9dd
Gamaliel Sirengo <sirengogamaliel@gmail.com>
2022-07-29 18:26:43 +0300

    update financial paths on base template

--- modules/urls.py
+++ modules/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('students/', views.students, name='students' ),
     path('exams/', views.exams, name='exams' ),
     path('cbc/', views.cbc, name='cbc' ),
+    path('timetable/', views.timetable, name='timetable' ),
 ]
